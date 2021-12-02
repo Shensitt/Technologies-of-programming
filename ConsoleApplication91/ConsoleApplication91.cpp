365a6a24511d5943911d493bac9c309deeb8dc42
shensitt <90465081+Shensitt@users.noreply.github.com>
2021-12-02 23:57:44 +0300

    Add files via upload

---
+++ ConsoleApplication91/ConsoleApplication91.cpp
@@ -0,0 +1,42 @@
+﻿#include <fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+	ifstream first_file("test1.txt");
+	if (!first_file) {
+		cout << "error while opening the file";
+		return 1;
+	}
+	
+	string new_file_name;
+	cout << "\nwrite the name of the new file: \n";
+	cin >> new_file_name;
+	ofstream newfile(new_file_name);
+	if (!newfile) {
+		cout << "error while creating/opening the file";
+		return 1;
+	}
+
+	string s;
+	while (!first_file.eof()) {
+		getline(first_file, s);
+		newfile << s << "\n";
+	}
+
+	/*char ch[1000];
+	for (int a = 0; a < 1000; a++) {
+		ch[a] = first_file.get();
+		newfile << ch[a];
+		if (first_file.eof()) { break;}
+	}*/
+	
+	
+	first_file.close();
+	cout << "\nWriting done\n";
+	newfile.close();
+	
+}
+
---
+++ ConsoleApplication92/ConsoleApplication92.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+const int length = 150;
+
+int main() {
+	ifstream first_file("test1.txt");
+	if (!first_file) {
+		cout << "error while opening the file";
+		return 1;
+	}
+	ofstream second_file("test2.txt");
+	if (!second_file) {
+		cout << "error while opening the file";
+		return 1;
+	}
+
+	char word[length];
+	char line[length];
+	cout << "write the word:\n";
+	cin >> word;
+
+	
+	while (first_file.getline(line, length)) {
+		if (strstr(line, word)) {
+
+			second_file << line << "\n";
+			
+		}
+
+	}
+	
+	
+	first_file.close();
+	second_file.close();
+}
