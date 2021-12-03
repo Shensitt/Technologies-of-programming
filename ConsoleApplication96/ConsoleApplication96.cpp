74f3441cd47c39fe55655594ceaa2ca37af82832
shensitt <90465081+Shensitt@users.noreply.github.com>
2021-12-03 16:12:22 +0300

    Add files via upload

---
+++ ConsoleApplication96/ConsoleApplication96.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+string filename;
+string out_filename;
+
+//int file_copy(string filename, string out_filename) {
+//	ifstream file(filename);
+//	if (!file) {
+//		cout << "error while opening the file" << endl;
+//	}
+//	int a = 0;
+//	char line[200];
+//	while (!file.eof()) {
+//		file.getline(line, 200);
+//		a++;
+//	}
+//	cout << a;
+//	file.seekg(0);
+//	ifstream out_file(out_filename);
+//	if (!out_file) {
+//		cout << "error while opening the file" << endl;
+//	}
+//	while (!out_file.eof()) {
+//		for (int n = 1; n <= a; n++) {
+//			file.getline(line, 200);
+//			out_file << n << "  " << line << endl;
+//		}
+//	}
+//	file.close();
+//	out_file.close();
+//	return 0;
+//}
+
+int main() {
+	/*file_copy("test1.txt", "test3.txt");
+	file_copy("test2.txt", "test3.txt");*/
+	
+	ifstream test1("test1.txt");
+		if (!test1) {
+			cout << "error while opening the file" << endl;
+		}
+		
+	ifstream test2("test2.txt");
+		if (!test2) {
+			cout << "error while opening the file" << endl;
+		}
+	ofstream test3("test3.txt");
+		if (!test3) {
+			cout << "error while opening the file" << endl;
+		}
+	int a = 0;
+	char line[200];
+	int prog = 0;
+
+	while (!test1.eof()) {
+		test1.getline(line, 200);
+		a++;
+	}
+	//cout << a;
+cout << "copying file1" << endl;
+cout << "[";
+	test1.seekg(0);
+	while (!test1.eof()) {
+		for(int n = 1;n<=a;n++){
+			test1.getline(line,200);
+			test3 <<n<< "  " << line<<endl;
+			if(prog<11){
+			cout << "=";
+			prog++;
+			Sleep(350);
+			}
+		}
+	}
+	cout << "]"<<endl<<"done"<<endl<<endl;
+	
+
+	test3 << endl;
+
+	a = 0; prog = 0;
+	//char line[200];
+	while (!test2.eof()) {
+		test2.getline(line, 200);
+		a++;
+	}
+	//cout << a;
+cout << "copying file2" << endl;
+cout << "[";
+	test2.seekg(0);
+	while (!test2.eof()) {
+		for (int n = 1; n <= a; n++) {
+			test2.getline(line, 200);
+			test3 << n << "  " << line << endl;
+			if (prog < 11) {
+				cout << "=";
+				Sleep(350);
+				prog ++;
+			}
+		}
+	}
+	cout << "]" << endl << "done" << endl << endl;
+	
+
+	test1.close();
+	test2.close();
+	test3.close();
+}
---
+++ ConsoleApplication97/ConsoleApplication97.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ifstream test1("test1.txt");
+	if (!test1) {
+		cout << "error while opening the file" << endl;
+	}
+	ifstream test2("test2.txt");
+	if (!test2) {
+		cout << "error while opening the file" << endl;
+	}
+
+	ofstream test3("test3.txt");
+	if (!test3) {
+		cout << "error while opening the file" << endl;
+	}
+////////////////////////////////////////////////////
+	char text1[3000];
+
+	int a = 0;
+	while (!test1.eof()) {
+		text1[a] = test1.get();
+		a++;
+	}
+	for (int i = a; i < 3000; i++) {
+		text1[i] = 0;
+	}
+	cout << text1 << endl << endl;
+
+
+	char text2[3000];
+
+	int b = 0;
+	while (!test2.eof()) {
+		text2[b] = test2.get();
+		b++;
+	}
+	for (int i = b; i < 3000; i++) {
+		text2[i] = 0;
+	}
+	cout << text2 << endl << endl; 
+	int word_max;
+	int sym_cnt = 0; int word_mass_index_1 = 0; //int word_mass_index_2 = 0;
+	char word1[20];
+	string word_mass[5000];
+	for (int aa = 0; aa < a; aa++) {
+		if (text1[aa] != ' ' && text1[aa] != '\n' && text1[aa] != '\t') {
+
+			word1[sym_cnt] = text1[aa];
+
+			sym_cnt++;
+			for (int aa = sym_cnt; aa < 20; aa++) {
+				word1[aa] = 0;
+			}
+
+			if (sym_cnt > 20) { break; }
+
+		}
+		else {
+			word_mass[word_mass_index_1] = word1;
+			sym_cnt = 0;
+			word_mass_index_1+=2;
+			word_max = word_mass_index_1;
+
+			/*if (word_mass_index % 2 == 0) {
+				if (word_mass_index != 0) {
+
+					test2 << word_mass[word_mass_index - 1] << ' ';
+					test2 << word_mass[word_mass_index - 2] << ' ';
+				}
+			}*/
+		}
+	}
+
+	/*string word_mass_2[1000]; */word_mass_index_1 = 1;
+	for (int aa = 0; aa < b; aa++) {
+		if (text2[aa] != ' ' && text2[aa] != '\n' && text2[aa] != '\t') {
+
+			word1[sym_cnt] = text2[aa];
+
+			sym_cnt++;
+			for (int aa = sym_cnt; aa < 20; aa++) {
+				word1[aa] = 0;
+			}
+
+			if (sym_cnt > 20) { break; }
+
+		}
+		else {
+			word_mass[word_mass_index_1] = word1;
+			sym_cnt = 0;
+			word_mass_index_1+=2;
+
+			/*if (word_mass_index % 2 == 0) {
+				if (word_mass_index != 0) {
+
+					test2 << word_mass[word_mass_index - 1] << ' ';
+					test2 << word_mass[word_mass_index - 2] << ' ';
+				}
+			}*/
+		}
+	}
+	//for(word_mass)
+	//test3<<word_mass_1[]
+	//////////////////////////////////////////////////
+	
+	for (word_mass_index_1=0;word_mass_index_1<word_max; word_mass_index_1++) {
+		test3 << word_mass[word_mass_index_1]<<endl;
+	}
+
+	test1.close();
+	test2.close();
+	test3.close();
+}
+
